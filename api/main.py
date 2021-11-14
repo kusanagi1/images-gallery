2d2cf395895a6708a50f1a9f84f32f73c51d6ac1
kusanagi <rebuiick@yahoo.com>
2021-11-14 18:09:24 -0500

    Enabled pylint and black code formatter in python

--- api/main.py
+++ api/main.py
@@ -2,10 +2,11 @@ from logging import DEBUG
 import requests, os
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv(dotenv_path=".env.local")
-UNSPLASH_URL="https://api.unsplash.com/photos/random"
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 DEBUG = os.environ.get("DEBUG") if os.environ.get("DEBUG") else False
 
 if not UNSPLASH_KEY:
@@ -13,24 +14,24 @@ if not UNSPLASH_KEY:
 
 app = Flask(__name__)
 app.config["DEBUG"] = DEBUG
+CORS(app)
+
 
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
-    headers = {
-        "Accept-Version": "v1",
-        "Authorization": "Client-ID " + UNSPLASH_KEY 
-    }
+    headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
     params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
-    
+
     data = response.json()
     return data
+
 
 # def hello():
 #     return "Hello, World!"
 
 # app.route("/")(hello)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=2222)
